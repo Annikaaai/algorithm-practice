5d7461835a8c4f052a98c8ed30c6f2de8a0bb9a8
Annikaaai <108407981+Annikaaai@users.noreply.github.com>
2025-09-08 13:44:50 +0300

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    String word1 = "Hello";
+    String word2 = "world";
+    String sentence = word1 + " " + word2;
+    System.out.println(word1 + " + " + word2 + " = " + sentence);
+  }
+}
