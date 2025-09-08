33a46df6ce6bdea8d8413760bc3ab4a4db879220
Annikaaai <108407981+Annikaaai@users.noreply.github.com>
2025-09-08 13:44:15 +0300

    Create c++1.cpp

---
+++ c++1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string word1 = "Hello";
+    string word2 = "world";
+    string sentence = word1 + " " + word2;
+    cout << word1 << " + "<< word2<< " = "<< sentence;
+
+}
